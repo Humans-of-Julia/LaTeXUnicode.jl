7a598c1d62173d91d3a920c04241de01c9109bad
Azzaare <jf@baffier.fr>
2021-05-04 16:49:07 +0900

    First dataset

--- src/LaTeXt.jl
+++ src/LaTeXt.jl
@@ -1,5 +1,12 @@
 module LaTeXt
 
-# Write your package code here.
+using CSV
+using DataFrames
+
+export df
+
+# From http://milde.users.sourceforge.net/LUCR/Math/data/unimathsymbols.txt
+df = DataFrame(CSV.File("data/unimathsymbols.txt"; comment="#", header = false, delim = '^'))
+rename!(df, [:code_point, :character, :latex, :class, :category, :requirements, :conflicts, :description])
 
 end
