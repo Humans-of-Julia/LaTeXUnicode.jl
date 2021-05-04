05fafb7e57cb2b596ff14092c2bece28d7d8fad9
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 11:34:49 +0000

    build based on 3d25926

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LaTeXt","category":"page"},{"location":"#LaTeXt","page":"Home","title":"LaTeXt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LaTeXt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LaTeXt]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LaTeXUniCode","category":"page"},{"location":"#LaTeXUniCode","page":"Home","title":"LaTeXUniCode","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LaTeXUniCode.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LaTeXUniCode]","category":"page"}]
 }
