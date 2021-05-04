89a32c28cd6fd788c1c4f6c1a4d41dc8cda2a45b
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 07:37:20 +0000

    build based on b337acf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LaTeXt","category":"page"},{"location":"#LaTeXt","page":"Home","title":"LaTeXt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LaTeXt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LaTeXt]","category":"page"}]
+}
